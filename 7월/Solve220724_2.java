5927c9b28644aa918cdf4caa3380b4f267a5b365
Hyung_Gyu_KIM <43024732+vellimole0621@users.noreply.github.com>
2022-07-24 00:04:00 +0900

    220724

---
+++ 7월/Solve220724_2.java
@@ -0,0 +1,13 @@
+package algorithm;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+public class Solve220724_2 {
+
+    public static void Main(String[] args){
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+        
+    }
+}
