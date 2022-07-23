863d1f0fc13fd96ded1352b01f3acdf22f00341d
Hyung_Gyu_KIM <43024732+vellimole0621@users.noreply.github.com>
2022-07-23 12:07:05 +0900

    220723

---
+++ 7월/Solve220723.java
@@ -0,0 +1,22 @@
+package algorithm;
+import java.util.*;
+
+public class Solve220723 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in); //scanner 객체 생성
+
+        while(sc.hasNextInt()){ //while 문(조건 성립할 때까지, 반복)
+
+            int A = sc.nextInt(); //A 입력
+            int B = sc.nextInt(); //B 입력
+
+            System.out.println(A+B); // A + B 합 출력
+            
+        }
+        sc.close(); 
+        }
+  
+        
+    }
+    
