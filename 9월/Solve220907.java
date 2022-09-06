75135fc56313f414794cb6f3bebbde6f1baa2f75
Hyung_Gyu_KIM <43024732+vellimole0621@users.noreply.github.com>
2022-09-07 02:06:40 +0900

    2455번

---
+++ 9월/Solve220907.java
@@ -0,0 +1,31 @@
+// 2455번 지능형 기차
+
+import java.util.*;
+
+public class Solve220907 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        // 최고로 많이 있는 정차역의 사람 수 초기화
+        int manyP = 0;
+
+        // 각 정차역의 사람 수 초기화
+        int nowP = 0;
+
+        // 4번의 정차역 반복
+        for(int i = 0; i < 4; i++){
+            int out = sc.nextInt();
+            int in = sc.nextInt();
+
+            nowP += (in - out);
+            if(nowP > manyP){
+                manyP = nowP;
+            }
+        }
+
+        // 결과 출력
+        System.out.println(manyP);
+        
+        sc.close();
+    }
+}
