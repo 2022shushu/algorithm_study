00588e54f0bdef4342cf9151539975ed2a071376
Hyung_Gyu_KIM <43024732+vellimole0621@users.noreply.github.com>
2022-09-18 12:02:35 +0900

    5522

---
+++ 9월/Solve220918.java
@@ -0,0 +1,20 @@
+// 번
+
+import java.util.*;
+
+public class Solve220918 {
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+
+    int re = 0;
+
+    while(sc.hasNext()){
+        int a = sc.nextInt();
+        re += a;
+    }
+
+    System.out.println(re);
+            sc.close();
+    }
+
+}
