6e637ab24aaf38fecbaa1918310c8dcb7d8d2f7a
Hyung_Gyu_KIM <43024732+vellimole0621@users.noreply.github.com>
2022-09-20 09:42:26 +0900

    15649번

---
+++ 9월/Solve220920.java
@@ -0,0 +1,21 @@
+// 15649 번
+
+import java.util.*;
+
+public class Solve220920 {
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+
+        // n 입력
+        int n = sc.nextInt();
+
+        if(n % 2 == 0){
+            System.out.println("CY");
+        }else{
+            System.out.println("SK");
+        }
+
+        sc.close();
+    }
+
+}
