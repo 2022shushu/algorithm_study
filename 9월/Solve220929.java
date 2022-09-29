998c657c44315c4485f7eb3e277a2ea1cd5672a0
Hyung_Gyu_KIM <43024732+vellimole0621@users.noreply.github.com>
2022-09-29 11:24:10 +0900

    11719번

---
+++ 9월/Solve220929.java
@@ -0,0 +1,16 @@
+// 11719번
+
+import java.util.*;
+
+public class Solve220929 {
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNext() != false){
+            System.out.println(sc.nextLine());
+        }
+
+        sc.close();
+    }
+
+}
